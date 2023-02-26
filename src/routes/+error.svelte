97f9b4438ac441cc34790575aad0d45d56a852c1
Quan Tran <minhquantran451@gmail.com>
2023-02-25 19:14:36 -0600

    Added 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<div class="h-screen w-screen flex flex-col justify-center items-center">
+	<h1 class="text-5xl font-semibold text-primary mb-2 md:mb-4">404</h1>
+	<p class="text-xl lg:text-2xl mb-8">Page Not Found</p>
+	<a href="/" class="btn btn-primary ">Back to home</a>
+</div>
